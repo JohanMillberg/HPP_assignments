359b78f13de61c3f3e8320aeadcd46ad4cb8da27
Kvarnberg6 <johanmillberg@me.com>
2022-01-26 16:36:18 +0100

    Added triang.c prototype

--- Assignment2/part1/triang.c
+++ Assignment2/part1/triang.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    printf("Main goes here");
+int element(int n, int k) {
+    int sum = 1;
+    if (n == 0) {
+        return sum;
+    }
+    for (int i = 0; i < k; i ++) {
+        sum *= (n-i)/(i+1);
+    }
+    return sum;
+}
+
+int main(int argc, char *argv[]) {
+    int i, j;
+    int el;
+    int n = atoi(argv[1]);
+    printf("Main goes here, %d \n", n);
+
+    for (j = 0; j < n; j++) {
+        for (i = 0; i < j; i++) {
+            el = element(j,n);
+            printf("%d", el);
+        }
+        printf("\n");
+    }
+
 }
