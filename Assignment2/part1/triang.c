14ce055e4416bc1467e023b33a05f60a35fc5159
Kvarnberg6 <johanmillberg@me.com>
2022-01-26 17:50:57 +0100

    Fixed invalid write error

--- Assignment2/part1/triang.c
+++ Assignment2/part1/triang.c
@@ -4,10 +4,10 @@
 int main(int argc, char *argv[]) {
     int n = atoi(argv[1]);
     int temp = 1;
-    int **triangle = (int **) malloc(n*sizeof(int*));
+    int **triangle = (int **) malloc(n*sizeof(int*)); // Allocates memory for a 2D array
 
     for (int i = 0; i < n; i++) {
-        triangle[i] = (int *) malloc(i*sizeof(int));
+        triangle[i] = (int *) malloc((i+1)*sizeof(int)); // Allocates memory for each row (array)
 
         for (int j = 0; j <= i; j++) {
             if (i == 0 || j == 0) {
